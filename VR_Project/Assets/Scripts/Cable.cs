12daeee085e724ec5f3457aeeb80caeb45d4d507
std::Gregwar <gregoirehirt@gmail.com>
2018-05-06 21:13:19 +0200

    cable cutting working

---
+++ VR_Project/Assets/Scripts/Cable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cable : MonoBehaviour {
+
+    Pliers pliers = null;
+    public Cables cables;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Pliers p = other.gameObject.GetComponent<Pliers>();
+        if (p) pliers = p;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        Pliers p = other.gameObject.GetComponent<Pliers>();
+        if (p) pliers = p;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Pliers p = other.gameObject.GetComponent<Pliers>();
+        if (p) pliers = null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (pliers)
+            print("La merde");
+		if(pliers && pliers.cutting)
+        {
+            cables.cut(this);
+        }
+	}
+}
