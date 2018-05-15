b4dabe44fa54446721553c8261acdacc6c43e6df
lromerio <lucio.romerio@epfl.ch>
2018-05-15 16:36:31 +0200

    Timer v1

---
+++ VR_Project/Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float remainingTime;
+    public static bool started;
+
+	// Use this for initialization
+	void Start () {
+        remainingTime = 600.0f;
+        started = false;
+    }
+
+    public static void StartTimer()
+    {
+        started = true;
+    }
+
+    public string GetTimeLeft()
+    {
+
+        TimeSpan t = TimeSpan.FromSeconds(remainingTime);
+
+        string s = string.Format("{0:D2}:{1:D2}:{2:D3}",
+                    t.Minutes,
+                    t.Seconds,
+                    t.Milliseconds);
+
+        return s;
+}
+	
+	// Update is called once per frame
+	void Update () {
+        if (started)
+            remainingTime -= Time.deltaTime;
+    }
+}
---
+++ VR_Project/Assets/Scripts/Timer/TimerCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class TimerEvent : UnityEvent<TimerCollision> { }
+
+public class TimerCollision : MonoBehaviour {
+
+    public TimerEvent startTimer;
+    private bool started;
+
+    private void Start()
+    {
+        started = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!started)
+        {
+            startTimer.Invoke(this);
+            started = true;
+        }
+    }
+}
